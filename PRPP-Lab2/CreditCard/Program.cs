92d44b5c9dd0482fc37f5e7b420c7a15a00b61f0
RomanYuts <yutsrom@gmail.com>
2022-04-03 17:30:00 +0300

    Add CreditCard class

---
+++ PRPP-Lab2/CreditCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CreditCard!");
+        }
+    }
+}
