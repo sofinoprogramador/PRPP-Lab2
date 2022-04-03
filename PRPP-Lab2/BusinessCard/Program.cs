148420aa4e4be8e6b363ab52c5af482a7cb74303
NazarMarukha <_cox_@ukr.net>
2022-04-03 11:47:49 +0300

    Add businessCard class

---
+++ PRPP-Lab2/BusinessCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello BusinessCard!");
+        }
+    }
+}
